7ca6e26d34eae86d9f8dd89b10244f0ebdcd0f21
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-08-26 11:32:31 +0530

    Create CompareStrings.java

---
+++ javaMethods/string/CompareStrings.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CompareStrings {
+    public static boolean compareUsingCharAt(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        System.out.println("CharAt Compare: " + compareUsingCharAt(s1, s2));
+        System.out.println("equals() Compare: " + s1.equals(s2));
+    }
+}
