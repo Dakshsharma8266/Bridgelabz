f364f8e60cff9d83f910626a5c6a4e1ae0a6032c
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-09-03 11:45:41 +0530

    Create Book.java

---
+++ javaconctructor/Book.java
@@ -0,0 +1,17 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    Book(String t, String a, double p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+}
