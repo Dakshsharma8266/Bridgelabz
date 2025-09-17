1f28b1a51a8c0b93405481392ce92079d6f739ca
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-09-17 11:23:33 +0530

    Create Person.java

---
+++ Inheritance/Person.java
@@ -0,0 +1,35 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Teacher extends Person {
+    String subject;
+
+    Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    void displayRole() {
+        System.out.println("Teacher: " + name + ", Subject: " + subject);
+    }
+}
+
+class Student extends Person {
+    String grade;
+
+    Student(String name, int age, String grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    void displayRole() {
+        System.out.println("Student: " + name + ", Grade: " + grade);
+    }
+}
